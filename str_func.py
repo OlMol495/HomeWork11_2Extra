2f995ae855113673574db9c82a128b812b7ea5e3
Olga Lebedeva <olga.s.lebedeva@outlook.com>
2023-09-17 16:17:55 +0900

    added str_title function to str_func.py

--- str_func.py
+++ str_func.py
@@ -1,3 +1,8 @@
 def str_func():
     text = input("Введите слово или текст\n")
     return text.upper()
+
+def str_title():
+    """capitalizes first letter of input text"""
+    text = input("Введите текст\n")
+    return text.title()
